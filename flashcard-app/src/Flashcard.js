f6a4e1096120d48b29ce57d71d75ea378632b570
George Wan <singonwan@gmail.com>
2020-05-13 02:12:37 -0400

    styled individual flashcard with css along with the flip animation

--- flashcard-app/src/Flashcard.js
+++ flashcard-app/src/Flashcard.js
@@ -3,8 +3,20 @@ import React, { useState } from 'react';
 export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false);
     return (
-        <div onClick={() => setFlip(!flip)}>
-            {flip ? flashcard.answer : flashcard.question}
+        <div
+            className={`card ${flip ? 'flip' : ''}`}
+            onClick={() => setFlip(!flip, console.log(flip))}
+        >
+            <div className="front">
+                {flashcard.question}
+                <div className="flashcard-options">
+                    {flashcard.mc.map((option) => {
+                        return <div className="flashcard-option">{option}</div>;
+                    })}
+                </div>
+            </div>
+
+            <div className="back">{flashcard.answer}</div>
         </div>
     );
 }
