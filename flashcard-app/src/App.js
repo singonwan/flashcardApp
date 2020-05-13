aa709a91720a09efce1265b82659c70762ff4182
George Wan <singonwan@gmail.com>
2020-05-13 00:42:11 -0400

    added sample data and passed those data into flashcardlist and flashcard component to display

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
 
 function App() {
-    return <h1>Hello World</h1>;
+    const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+    return <FlashcardList flashcards={flashcards} />;
 }
+
+const SAMPLE_FLASHCARDS = [
+    {
+        id: 1,
+        question: 'what is 2 + 2?',
+        answer: '4',
+        mc: ['2', '3', '4', '5'],
+    },
+    {
+        id: 2,
+        question: 'what is 3 + 3?',
+        answer: '6',
+        mc: ['2', '3', '4', '6'],
+    },
+    {
+        id: 3,
+        question: 'what is 4 + 4?',
+        answer: '8',
+        mc: ['2', '8', '4', '5'],
+    },
+];
 
 export default App;
