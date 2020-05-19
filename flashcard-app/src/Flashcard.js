2ba9aaf16cedf9fda8c85c31a750cd68cef71dfc
George Wan <singonwan@gmail.com>
2020-05-19 02:15:15 -0400

    added JS in flashcard with useRef and useEffect to dynamically adjust set height and adjust height when we resize the window

--- flashcard-app/src/Flashcard.js
+++ flashcard-app/src/Flashcard.js
@@ -1,13 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Flashcard({ flashcard }) {
     const [flip, setFlip] = useState(false);
+    const [height, setHeight] = useState('initial');
+
+    const frontEl = useRef();
+    const backEl = useRef();
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height;
+        const backHeight = backEl.current.getBoundingClientRect().height;
+        setHeight(Math.max(frontHeight, backHeight, 100));
+    }
+
+    useEffect(setMaxHeight, [
+        flashcard.question,
+        flashcard.answer,
+        flashcard.options,
+    ]);
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight);
+        return () => window.removeEventListener('resize', setMaxHeight);
+    }, []);
+
     return (
         <div
             className={`card ${flip ? 'flip' : ''}`}
+            style={{ height: height }}
             onClick={() => setFlip(!flip, console.log(flip))}
         >
-            <div className="front">
+            <div className="front" ref={frontEl}>
                 {flashcard.question}
                 <div className="flashcard-options">
                     {flashcard.options.map((option) => {
@@ -20,7 +42,9 @@ export default function Flashcard({ flashcard }) {
                 </div>
             </div>
 
-            <div className="back">{flashcard.answer}</div>
+            <div className="back" ref={backEl}>
+                {flashcard.answer}
+            </div>
         </div>
     );
 }
